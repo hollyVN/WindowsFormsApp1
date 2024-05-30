eb202a0f0c7d5a01fbe983411090d217410bef1f
hollyVN <122336142+hollyVN@users.noreply.github.com>
2024-05-31 00:23:55 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReadFile = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnExportExcel = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnReadFile
+            // 
+            this.btnReadFile.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReadFile.ForeColor = System.Drawing.Color.Blue;
+            this.btnReadFile.Location = new System.Drawing.Point(110, 124);
+            this.btnReadFile.Name = "btnReadFile";
+            this.btnReadFile.Size = new System.Drawing.Size(191, 72);
+            this.btnReadFile.TabIndex = 0;
+            this.btnReadFile.Text = "READFILE";
+            this.btnReadFile.UseVisualStyleBackColor = true;
+            this.btnReadFile.Click += new System.EventHandler(this.btnReadFile_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnStop.Location = new System.Drawing.Point(323, 124);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(191, 72);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnExportExcel
+            // 
+            this.btnExportExcel.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExportExcel.ForeColor = System.Drawing.Color.Green;
+            this.btnExportExcel.Location = new System.Drawing.Point(536, 124);
+            this.btnExportExcel.Name = "btnExportExcel";
+            this.btnExportExcel.Size = new System.Drawing.Size(191, 72);
+            this.btnExportExcel.TabIndex = 0;
+            this.btnExportExcel.Text = "EXPORTtoEXCEL";
+            this.btnExportExcel.UseVisualStyleBackColor = true;
+            this.btnExportExcel.Click += new System.EventHandler(this.btnExportExcel_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "english",
+            "vietnamese"});
+            this.comboBox1.Location = new System.Drawing.Point(613, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(175, 28);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Language select";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnExportExcel);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnReadFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReadFile;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnExportExcel;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.IO; // to create a stream for file
+using System.Net; // for request and respond with http protocol
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using ExcelDataReader;
+using System.Configuration; // this package to reflect the server
+using ClosedXML.Excel; // this is a library for working with file *.xlsx
+using System.Globalization;
+using System.Resources;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private ResourceManager resourceManager;
+        public Form1()
+        {
+            InitializeComponent();
+            CultureInfo culture = CultureInfo.CurrentCulture;
+           // SetLanguage("en_US");
+        }
+       
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // set english is default
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new CultureInfo("en_US");
+
+            
+        }
+
+        string excelFilePath = "sample.xlsx";
+        string sqlServerStr = @"Data Source=DESKTOP-I6017GJ\SQLSERVER;Initial Catalog=SampleTest;Integrated Security=True";
+       // string serverUrl = ConfigurationManager.AppSettings["ServerUrl"];
+        
+
+        private void btnReadFile_Click(object sender, EventArgs e)
+        {
+            using (var stream = File.Open(excelFilePath,FileMode.Open,FileAccess.Read)) // create a stream use System.IO push filepath into
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))// create a reader and push stream into
+                {
+                    bool isFirstRow = true;
+                    do
+                    {
+                        while (reader.Read())
+                        {
+                            if (isFirstRow)
+                            {
+                                isFirstRow = false;
+                                continue;
+                            }
+                            string data = reader.GetString(0);// read data from column 0 in fileexcel
+
+                            // after get data will connect to sql server
+                            using (SqlConnection sqlConn = new SqlConnection(sqlServerStr))
+                            {
+                                sqlConn.Open();
+                                
+                                string sqlCmd = "MERGE INTO dbo.matches AS target " +
+                                                        "USING (SELECT @epc_code AS epc_code) AS source " +
+                                                        "ON target.epc_code = source.epc_code " +
+                                                        "WHEN MATCHED THEN " +
+                                                        "   UPDATE SET updated_at = GETDATE() " +
+                                                        "WHEN NOT MATCHED THEN " +
+                                                        "   INSERT (created_at, updated_at,ri_date, epc_code) " +
+                                                        "VALUES  (GETDATE(), GETDATE(), GETDATE(), @epc_code);";
+                                using (SqlCommand cmd = new SqlCommand(sqlCmd, sqlConn))
+                                {
+                                    cmd.Parameters.AddWithValue("@epc_code", data);
+                                    
+                                    cmd.ExecuteNonQuery();
+                                }
+                                // send data to server use HTTP POST request
+                                //var postData = "data=" + data; // this data we already declare
+                                //var dataBytes = Encoding.UTF8.GetBytes(postData);
+
+                                //var request = (HttpWebRequest)WebRequest.Create(serverUrl);
+                                //request.Method = "POST";
+                                //request.ContentType = "application/x-www-form-urlencoded";
+                                //request.ContentLength = dataBytes.Length;
+
+                                //using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                                //{
+                                //    streamWriter.Write(postData);
+                                //}
+
+                                //var response = (HttpWebResponse)request.GetResponse();
+                                //var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                            }
+
+                        }
+                    } while (reader.NextResult());
+                }
+            }
+            MessageBox.Show("Bạn đã import dữ liệu thành công lên database");
+        }
+
+        string exportQuery = "select ri_date, epc_code from dbo.matches";
+        private void btnExportExcel_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlConn = new SqlConnection(sqlServerStr);
+            sqlConn.Open();
+            SqlCommand cmd = new SqlCommand(exportQuery, sqlConn);
+            // truyền data từ sql server vào một đầu đọc reader
+            SqlDataReader reader = cmd.ExecuteReader();
+            var workbook = new XLWorkbook(); // create a workbook
+            var worksheet = workbook.Worksheets.Add("ExportData"); // create a worksheet
+            worksheet.Cell(1, 1).Value = "ri_date";// header of column 1
+            worksheet.Cell(1, 2).Value = "epc_code"; // header of column 2
+
+            // tao loop truyền vào file
+            int row = 2;
+            while (reader.Read())
+            {
+                worksheet.Cell(row, 1).Value = (DateTime)reader["ri_date"];
+                worksheet.Cell(row, 2).Value = (string)reader["epc_code"];
+                row++;
+            }
+            workbook.SaveAs("exportEpcCode.xlsx");
+            MessageBox.Show("Export thành công");
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            string selectedLanguage = comboBox.SelectedItem.ToString();
+            if(selectedLanguage == "english")
+            {
+                SetLanguage("en_US");
+            }
+            else
+            {
+                SetLanguage("vi_VN");
+            }
+            
+        }
+        private void SetLanguage(string lang)
+        {
+            CultureInfo culture = CultureInfo.CreateSpecificCulture(lang);
+            ResourceManager rm = new ResourceManager("WindowsFormsApp1.Properties.Resources", typeof(Form1).Assembly);
+
+            btnReadFile.Text = rm.GetString("ReadFile", culture);
+            btnStop.Text = rm.GetString("Stop", culture);
+            btnExportExcel.Text = rm.GetString("ExportExcel", culture);
+
+            
+        }
+    }
+}
